43223b23352a412b52996133161f9501e722b2e5
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-25 18:43:19 +0530

    day5 Logical Problems

--- src/day6LogicalProblem/StopWatch.java
+++ src/day6LogicalProblem/StopWatch.java
@@ -1,15 +1,33 @@
 package day6LogicalProblem;
 
+import java.util.Scanner;
+
 public class StopWatch {
 
 	public static void main(String[] args) {
-		
-		long StartTime = System.currentTimeMillis();              
-		System.out.println("Starting time is " +StartTime);
+		/**
+		 * taking start time from system by entering 1 using scanner taking end time
+		 * from system by entering 2 using scanner calculating elapsed time (ENDTIME-
+		 * START TIME) elapsed will be in mili seconds dividing elapsed by 1000 to get
+		 * result in seconds
+		 * 
+		 */
+		long elapsed = 0;
+		long StartTime = System.currentTimeMillis();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Press 1 to start the stop watch ");
+		int N = input.nextInt();
+		if (N == 1) {
+			System.out.println("Starting time is " + StartTime);
+		}
 		long StopTime = System.currentTimeMillis();
-		System.out.println("End time is " +StopTime);
-
-	
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Press 2 to start the stop watch ");
+		int M = input2.nextInt();
+		if (M == 2) {
+			System.out.println("Starting time is " + StopTime);
+		}
+		elapsed = StopTime - StartTime;
+		System.out.println("Elapsed is " + (elapsed / 1000) + " Seconds");
 	}
-
 }
